c6a58e6e5897627942b011b8848ddd84954e8274
Thai Tin Khang <kinyiasgit@gmail.com>
2025-03-30 21:44:10 +0700

    Fix UI

--- data/Lookup.jsx
+++ data/Lookup.jsx
@@ -1,53 +1,3 @@
-// //Dung cho trang price
-// const LOOKUP_DATA = {
-//     DEFAULT_FILE: {
-//       name: 'default.txt',
-//       size: '0KB',
-//       type: 'text/plain',
-//     },
-  
-//     DEPENDANCY: {
-//       required: ['React', 'Next.js', 'Convex', 'Clerk'],
-//       optional: ['TailwindCSS', 'ShadCN'],
-//     },
-  
-//     PRICING_DESC: 'Bắt đầu với một tài khoản miễn phí để tăng tốc quy trình làm việc trên nền tảng của chúng tôi.',
-  
-//     PRICING_OPTIONS: [
-//       {
-//         name: 'Miễn phí',
-//         tokens: '50K',
-//         value: 50000,
-//         desc: 'Dành cho người dùng cá nhân muốn trải nghiệm cơ bản.',
-//         price: 'Miễn phí',
-//       },
-//       {
-//         name: 'Cơ bản',
-//         tokens: '200K',
-//         value: 200000,
-//         desc: 'Phù hợp với cá nhân hoặc nhóm nhỏ, sử dụng trung bình.',
-//         price: '49K VND / tháng',
-//       },
-//       {
-//         name: 'Nâng cao',
-//         tokens: '500K',
-//         value: 500000,
-//         desc: 'Dành cho các doanh nghiệp nhỏ, yêu cầu sử dụng nhiều hơn.',
-//         price: '149K VND / tháng',
-//       },
-//       {
-//         name: 'Doanh nghiệp',
-//         tokens: '1M+',
-//         value: 1000000,
-//         desc: 'Gói không giới hạn, phù hợp với doanh nghiệp lớn.',
-//         price: 'Liên hệ',
-//       }
-//     ],
-    
-//   };
-  
-//   export default LOOKUP_DATA;
-  
 // Dùng cho trang price
 const LOOKUP_DATA = {
   DEFAULT_FILE: {
@@ -94,7 +44,7 @@ const LOOKUP_DATA = {
       tokens: '1M+',
       value: 1000000,
       desc: 'Gói không giới hạn, phù hợp với doanh nghiệp lớn.',
-      price: 'Liên hệ',
+      price: '298K VND/tháng',
       price_f: 298000,  // Giá liên hệ không có số cụ thể
     }
   ],
